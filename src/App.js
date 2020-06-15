29fd8b3b52ead56b4cf0c3f5aedb026a53735b49
你çclang1996 <>
2020-06-15 17:04:14 +0800

    涟漪按钮

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useRef} from 'react';
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    const [active, setActive] = useState(false)
+    const [deltX,setDeltaX]= useState(0)
+    const [deltY,setDeltaY]= useState(0)
+    const btnRef = useRef(null)
+    const onClick = (e) => {
+        let {x, y} = btnRef.current.getBoundingClientRect()
+        let{clientX,clientY} = e
+        let deltaX = clientX - x-10
+        let deltaY = clientY - y-10
+        setActive(true)
+        setDeltaX(deltaX)
+        setDeltaY(deltaY)
+    }
+    const onAnimationEnd =(e)=>{
+        setActive(false)
+    }
+    return (
+        <div>
+        <button
+        className = "btn"
+        onClick = {onClick}
+        ref = {btnRef}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	        <span className="fuck">123</span>
+        {active === true ?<span
+            onAnimationEnd = {onAnimationEnd}
+            className = "circle"
+            style={{left:deltX,top:deltY}}/>:''}
+        </button>
+    < /div>
+)
 }
-
 export default App;
